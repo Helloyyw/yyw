059c58d23952b89eaa04a298c0b2014776e2068e
yyw <1475556814@qq.com>
2019-04-15 13:32:30 +0800

    第一次测试

---
+++ src/main/java/com/xmcc/springboot_demo/controller/LogbackTest1.java
@@ -0,0 +1,15 @@
+package com.xmcc.springboot_demo.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class LogbackTest1 {
+
+
+}
---
+++ src/main/java/com/xmcc/springboot_demo/test1.java
@@ -0,0 +1,12 @@
+package com.xmcc.springboot_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test1 {
+    @GetMapping("/hello")
+    public  String hello(){
+        return "你好！springboot";
+    }
+}
